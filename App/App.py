edce68c953269b49cda47a4f5669659baf63185e
konetisindhu23 <163759049+konetisindhu23@users.noreply.github.com>
2025-04-16 01:50:18 +0530

    Update App.py

--- App/App.py
+++ App/App.py
@@ -2,6 +2,7 @@
 
 
 ###### Packages Used ######
+import getpass
 import psycopg2
 import streamlit as st # core package used in this project
 import nltk
@@ -224,7 +225,7 @@ def run():
         sec_token = secrets.token_urlsafe(12)
         host_name = socket.gethostname()
         ip_add = socket.gethostbyname(host_name)
-        dev_user = os.getlogin()
+        dev_user = getpass.getuser()
         os_name_ver = platform.system() + " " + platform.release()
         g = geocoder.ip('me')
         latlong = g.latlng
